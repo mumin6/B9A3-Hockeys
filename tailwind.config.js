f40c049aad424855eab0af7ef5bf350863de224b
mumin6 <am.mumin99@outlook.com>
2024-01-24 19:55:39 +0600

    finished main

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    important: true,
+    theme: {
+      fontFamily: {
+        display: ['Manrope','sans-serif'],
+        body: ['Manrope','sans-serif'],
+      },
+      extend: {
+        colors: {
+          
+        },
+       
+      }
+    },
+    variants: {
+      opacity: ['responsive', 'hover']
+    }
+  }
